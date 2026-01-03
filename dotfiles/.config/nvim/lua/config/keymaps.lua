1a22501510f7c955b5a0e91deac8f093348ea5cd
Raphael Andrews <raphaelandrews95@gmail.com>
2026-01-03 18:23:31 -0300

    update

--- dotfiles/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>yc", function()
+  local d = vim.diagnostic.get(0, { lnum = vim.fn.line('.') - 1 })
+  if d and d[1] then
+    vim.fn.setreg("+", d[1].message)
+    vim.notify("Diagnostic copied")
+  else
+    vim.notify("No diagnostic here", vim.log.levels.WARN)
+  end
+end, { desc = "Yank diagnostic" })
