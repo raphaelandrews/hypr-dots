7ffb0ec7eefbe3ad2d84e1c4b30fb5a83e9745c9
raphaelandrews <hey@andrews.sh>
2026-09-05 00:48:03 -0300

    update

--- dotfiles/.config/hypr/looknfeel.lua
+++ dotfiles/.config/hypr/looknfeel.lua
@@ -14,16 +14,12 @@
 -- })
 
 -- https://wiki.hypr.land/Configuring/Basics/Variables/#decoration
--- hl.config({
---   decoration = {
---     -- Use round window corners.
---     rounding = 8,
---
---     -- Dim unfocused windows (0.0 = no dim, 1.0 = fully dimmed).
---     dim_inactive = true,
---     dim_strength = 0.15,
---   },
--- })
+hl.config({
+  decoration = {
+    -- Use round window corners.
+    rounding = 4,
+  },
+})
 
 -- https://wiki.hypr.land/Configuring/Basics/Variables/#animations
 -- hl.config({
@@ -38,6 +34,17 @@ hl.config({
   layout = {
     -- Avoid overly wide single-window layouts on wide screens.
     single_window_aspect_ratio = { 1.66, 1 },
+  },
+})
+
+-- Dwindle layout.
+-- https://wiki.hypr.land/Configuring/Dwindle-Layout/
+hl.config({
+  dwindle = {
+    split_width_multiplier = 1.6,
+    preserve_split = true,
+    force_split = 2,
+    default_split_ratio = 1.2,
   },
 })
 
